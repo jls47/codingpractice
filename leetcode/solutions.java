8bf247d111ec23cf56b030728d182a709a66fa27
jls47 <lukeschaefer@gmail.com>
2023-10-17 23:06:20 -0700

    Jump Game

--- leetcode/solutions.java
+++ leetcode/solutions.java
@@ -183,4 +183,43 @@ public class solutions {
             }
         }
     }
+
+    /*
+	Jump Game
+	You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+	Return true if you can reach the last index, or false otherwise.
+    */
+
+    //recursive solution.  Recursive calls in a loop going down
+    public boolean canJump(int[] nums) {
+        if(nums.length == 1) {
+            return true;
+        }
+        if(nums.length == 2) {
+            return nums[0] >= 1;
+        }
+        return findPath(nums, nums.length - 1);
+    }
+
+    private boolean findPath(int[] nums, int index) {
+        if(index == 0) {
+            return true;
+        }
+        int distance = 1;
+        boolean found = false;
+        for(int i = index - 1; i >= 0; i--) {
+            if(nums[i] >= distance) {
+                found = true;
+                break;
+            }
+            distance ++;
+        }
+        if(found) {
+            return findPath(nums, index - distance);
+        }
+        return false;
+    }
+
+    
 }
